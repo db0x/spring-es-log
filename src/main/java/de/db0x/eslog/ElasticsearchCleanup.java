994af4d24d7ef018e14dc066b57e3c141b973991
db0x <thomas@schwerdtzone.de>
2016-01-17 19:51:10 +0100

    prepare index-cleanup

--- src/main/java/de/db0x/eslog/ElasticsearchCleanup.java
+++ src/main/java/de/db0x/eslog/ElasticsearchCleanup.java
@@ -1,13 +1,9 @@
 package de.db0x.eslog;
 
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
-
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
 
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
@@ -15,16 +11,21 @@ import org.elasticsearch.common.hppc.cursors.ObjectCursor;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 //import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
-
+@EnableAsync
 public class ElasticsearchCleanup {
 
 	private final static Logger LOG = LoggerFactory.getLogger(ElasticsearchCleanup.class);
+	
+	@Autowired
+	private IndexCleaner ic;
 
 	//@Scheduled(fixedRate = 5000)
 	@PostConstruct
@@ -38,43 +39,14 @@ public class ElasticsearchCleanup {
 			for (ObjectCursor<String> key : indexes.keys()) {
 				if ( Utils.indexNameMatch(key.value, "")) {
 					Date created = new Date(indexes.get(key.value).creationDate());
-					if ( Utils.addDays(null, -5).after(created) && client.prepareCount(key.value).get().getCount() > 0 ) {
+					if ( Utils.addDays( null, - 3 ).after(created) && client.prepareCount(key.value).get().getCount() > 0 ) {
 						LOG.info("cleanup index "+ key.value+" ["+ client.prepareCount(key.value).get().getCount()+"]");
-						try {
-							clean( client, key.value );
-						} catch ( Exception e) {
-							LOG.error("unable to clean index "+ key.value+" "+ e.getMessage());
-						}
-					}
-					if ( client.prepareCount(key.value).get().getCount() == 0 ) {
-						LOG.info("delete index " + key.value+ " because it is empty" );
-						try {
-							delete(client, key.value);
-						} catch ( Exception e ) {
-							LOG.error("unable to delete index "+ key.value+" "+ e.getMessage());
-						}
+						ic.clean(  key.value );
 					}
 				}
 			}
 			if ( LOG == null ) {
 			}
 		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	private void clean( TransportClient client, String indexName ) {
-		SearchResponse response = client.prepareSearch().execute().actionGet();
-		
-		response.getHits().iterator().next().getId();
-		
-		client.prepareDeleteByQuery(indexName)
-        .setQuery(termQuery("Application", ""))
-        .execute()
-        .actionGet();
-						
-	}
-	
-	private void delete( TransportClient client, String indexName ) {
-		client.admin().indices().delete(new DeleteIndexRequest(indexName)).actionGet();
 	}
 }
---
+++ src/main/java/de/db0x/eslog/IndexCleaner.java
@@ -0,0 +1,59 @@
+package de.db0x.eslog;
+
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.search.SearchHit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+@Component
+public class IndexCleaner {
+
+	private final static Logger LOG = LoggerFactory.getLogger(IndexCleaner.class);
+
+	@Async
+	public void clean(String indexName) {
+		try (TransportClient client = new TransportClient()) {
+
+			client.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+
+			SearchResponse response = client
+					.prepareSearch(indexName).setQuery( termQuery("application", "ic") )
+					.setFrom(0)
+					.setSize(60)
+					.execute()
+					.actionGet();
+
+			for (SearchHit sh : response.getHits().getHits()) {
+				LOG.info(sh.getId());
+			}
+			
+			if ( client.prepareCount(indexName).get().getCount() == 0 ) {
+				LOG.info("delete index " + indexName+ " because it is empty" );
+				try {
+					delete(client, indexName);
+				} catch ( Exception e ) {
+					LOG.error("unable to delete index "+ indexName+" "+ e.getMessage());
+				}
+			}
+
+		} catch (Exception e) {
+			LOG.error("unable to clean index "+ indexName+ " "+ e.getMessage());
+		
+		}
+		// client.prepareDeleteByQuery(indexName)
+		// .setQuery(termQuery("Application", ""))
+		// .execute()
+		// .actionGet();
+	}
+
+	private void delete(TransportClient client, String indexName) {
+		client.admin().indices().delete(new DeleteIndexRequest(indexName)).actionGet();
+	}
+
+}
